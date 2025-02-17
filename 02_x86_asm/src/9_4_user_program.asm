b47a7bad1d5441ff7e5dac99cabf448835abe5a7
Yunyan <708931190@qq.com>
2025-02-17 17:40:18 +0800

    用户程序加载流程

--- 02_x86_asm/src/9_4_user_program.asm
+++ 02_x86_asm/src/9_4_user_program.asm
@@ -32,8 +32,8 @@ SECTION code_seg_1 align=16 vstart=0
 		inc bx
 		jmp display_string
 		
-		.exit_string:
-			ret
+	.exit_string:
+		ret
 	
 	;过程：显示单个字符
 	;输入：cl=单个字符ascii码
@@ -62,6 +62,27 @@ SECTION code_seg_1 align=16 vstart=0
 		mov bx,ax			;BX=光标位置16位数
 		
 		;判断是否为回车字符
+		;回车字符：应当把光标移动到当前行的行首
+		;做法：当前光标位置除以80，得到的商就是当前行的行号，然后在乘以80就是当前行首的光标数值
+		;示意图如下：
+		;0行：0   ---------- 79
+		;1行：80  ---------- 159
+		;2行：160 ---------- 239
+		;假设光标位置是220，除以80得到商（行号）=2，再乘以80，得到160，证明了这是行首
+		cmp 0x0d
+		jnz .put_0a		;不是，再判断是不是换行符
+		mov ax,bx
+		mov bl,80		
+		div bl			;除以80得到行号,这是16位除法，商放在AL中
+		mul bl			;此时，AX中存放的就是当前行首的光标值
+		mov bx,ax
+		jmp .set_cursor	;跳转到设置光标
+		
+	.put_0a:
+		cmp cl 0x0a			;是不是换行符
+		jnz .put_other		;不是，正常显示字符
+		add bx,80			;是换行符就+1行
+		jmp .roll_screnn	;如果光标原先就在屏幕最后一行，那么应该根据情况滚屏
 		 
 		pop es
         pop ds
@@ -71,6 +92,50 @@ SECTION code_seg_1 align=16 vstart=0
         pop ax
 
         ret
+	
+	;正常显示可打印字符
+	;光标占用一个字符的位置，一个字符=2个字节，所以：光标在显存中的偏移地址=字符位置*2=下一个字符的位置
+	.put_other:
+		mov ax,0xb800		;显存地址
+		mov es,ax
+		shl bx,1			;左移一位相当于乘以2，因为这里bx中的值是二进制，如果是16进制，那么左移一位相当于乘以4
+		mov [es:bx],cl		;写入所显示的字符
+		
+		shr bx,1			;下一个字符位置，将字节偏移地址恢复为字符位置
+		add bx,1			;字符位置+1
+	
+	;滚屏，实际上就是将第2-25的内容整体往上提一行
+	;也就是把每一行都往上移一行，为了提高效率，使用rep movsw完成传送工作
+	.roll_screnn:
+		cmp bx,2000
+		jl .set_cursor		;如果光标小于2000，没有超出屏幕显示，反之，需要执行滚动屏幕内容
+		
+		push bx				;待会要用到bx寄存器，所以先压栈保存
+		
+		mov ax,0xb800
+		mov ds,ax
+		mov es,ax
+		cld					;clear direction flag,清除方向标志位
+							;会设置标志寄存器中DF=0，串操作指令会按照地址递增的方向处理内存
+		mov si,0xa0			;源位置0xa0=160，从屏幕第2行第1列的位置开始
+		mov di,0x00			;目标位置从0x00处开始，屏幕第1行第1列
+		mov cx,1920			;1920=24行*每行80个字符*每个字符的占用字节数 / 字(2字节)
+		rep movsw			;rep=重复执行,movsw = mov string word,
+							; DS:SI -> ES:DI
+	
+	;由于屏幕最后一行还保持的原来的内容，需要清除最后一行
+		mov bx,3840			;屏幕上第25行第1列在显存中的偏移位置
+		mov cx,80
+	.cls:
+		mov word [es:bx],0x0720		;黑底白字的空白字符
+		add bx,2
+		loop .cls
+		
+		pop bx
+		sub bx,80		;滚屏后，移动到最后一行的行首，因为之前判断是不是换行符已经加了80
+	
+	.set_cursor:
+		
 	
 	start:
 		mov ax,[stack_seg_item]			;设置自己的栈段
